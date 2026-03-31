7df32870be719b7d370baa9ce489035f1429e7a3
alldigitalgames <cagrimuratkose@gmail.com>
2026-03-31 14:10:57 +0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Supabase URL veya Anon Key eksik! Vercel Environment Variables kontrol et.');
+  throw new Error('Supabase URL veya Anon Key eksik! Vercel Environment Variables kısmını kontrol et.');
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
